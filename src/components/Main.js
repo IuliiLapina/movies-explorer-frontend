134af6b7caac770816fe43aae8599c8a72a4211c
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-09-23 16:11:04 +0300

    Component Promo
    Promo — компонент с вёрсткой баннера страницы «О проекте».

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Promo from './Promo'
+
+function Main() {
+  return (
+    <main className="content">
+      <Promo/>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Promo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Promo() {
+  return (
+    <section className="promo">
+      <div className="promo__bakground">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Promo;
